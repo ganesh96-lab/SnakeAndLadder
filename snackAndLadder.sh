1951ac7e992fbd27dc7d4d3371e29d5dec743d4f
ganesh96-lab <ganeshghodke7050@gmail.com>
2020-03-26 00:53:38 -0400

    Added: UC4 Repeat dice till player goes to 100

--- snackAndLadder.sh
+++ snackAndLadder.sh
@@ -7,24 +7,30 @@ START=0
 NO_PLAY=0
 SNAKE=1
 LADDER=2
-
+WINNING=100
 echo "starting position of player : $START"
-diceValue=$((RANDOM%6 + 1 ))
-echo "Dice value is $diceValue"
 
+playerCurrPos=$START
 function play(){
-   option=$((RANDOM % 3))
-   case $option in
-      $NO_PLAY)
-         echo "NO play.....";;
-      $SNAKE)
-         echo "snake......."
-         ((playerCurrPos-=$diceValue));;
-      $LADDER)
-         echo "Ladder....."
-         ((playerCurrPos+=$diceValue));;
-   esac
-   echo "player current position : $playerCurrPos"
+
+	while [ $playerCurrPos -lt $WINNING ]
+	do
+		diceValue=$((RANDOM%6 + 1 ))
+		echo "Dice value is $diceValue"
+
+   	option=$((RANDOM % 3))
+   	case $option in
+      	$NO_PLAY)
+         	echo "NO play.....";;
+      	$SNAKE)
+         	echo "snake......."
+         	((playerCurrPos-=$diceValue));;
+      	$LADDER)
+         	echo "Ladder....."
+         	((playerCurrPos+=$diceValue));;
+   	esac
+   	echo "player current position : $playerCurrPos"
+	done
 }
 
 play
