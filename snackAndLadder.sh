ce40ba242f3df6b29224a85d0352d766f906c355
ganesh96-lab <ganeshghodke7050@gmail.com>
2020-03-26 00:31:47 -0400

    ADDED:uc3 option

--- snackAndLadder.sh
+++ snackAndLadder.sh
@@ -2,5 +2,30 @@
 
 echo "~~~~~~~~~~~START SNAKE AND LADDER~~~~~~~~~~~~~~~~~~~~"
 
+#CONSTANT
+START=0
+NO_PLAY=0
+SNAKE=1
+LADDER=2
+
+echo "starting position of player : $START"
 diceValue=$((RANDOM%6 + 1 ))
 echo "Dice value is $diceValue"
+
+function play(){
+   option=$((RANDOM % 3))
+   case $option in
+      $NO_PLAY)
+         echo "NO play.....";;
+      $SNAKE)
+         echo "snake......."
+         ((playerCurrPos-=$diceValue));;
+      $LADDER)
+         echo "Ladder....."
+         ((playerCurrPos+=$diceValue));;
+   esac
+   echo "player current position : $playerCurrPos"
+}
+
+play
+
